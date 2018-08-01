9a7ae4b70f4bc82246036d8511e4312ae44e00d6
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-08-01 20:10:12 +0300

    Create world_count.go

---
+++ interview/world_count/world_count.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+  "sort"
+)
+
+type sortedMap struct {
+  m map[string]int
+  s []string
+}
+
+func (sm *sortedMap) Len() int {
+  return len(sm.m)
+}
+
+func (sm *sortedMap) Less(i, j int) bool {
+    if sm.m[sm.s[i]] == sm.m[sm.s[j]] {
+      return sm.s[i] < sm.s[j]
+  }
+  return sm.m[sm.s[i]] > sm.m[sm.s[j]]
+}
+
+func (sm *sortedMap) Swap(i, j int) {
+  sm.s[i], sm.s[j] = sm.s[j], sm.s[i]
+}
+
+func sortedKeys(m map[string]int) []string {
+  sm := new(sortedMap)
+  sm.m = m
+  sm.s = make([]string, len(m))
+  i := 0
+  for key, _ := range m {
+    sm.s[i] = key
+    i++
+  }
+  sort.Sort(sm)
+  return sm.s
+}
+
+func print_word(words map[string]int) {
+    for _, word := range sortedKeys(words) { 
+    fmt.Println(word, words[word])
+  }
+}
+
+func world_count(text string) map[string]int {
+    words := make(map[string]int)
+    for _, field := range strings.Split(text, " ") {
+        words[strings.ToLower(field)]++
+    }
+    return words
+}
+
+func main() {
+    text := "This is a big apple tree I love big big apple 42"
+  words := world_count(text)
+  print_word(words)
+}
