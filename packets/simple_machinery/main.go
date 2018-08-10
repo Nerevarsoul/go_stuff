0bfbb76016e19bebcc71c12976028ff2beb66318
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-08-10 22:31:46 +0300

    Add simple machinery

---
+++ packets/simple_machinery/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+  "fmt"
+  "time"
+
+  "github.com/RichardKnop/machinery/v1/config"
+  "github.com/RichardKnop/machinery/v1"
+  "github.com/RichardKnop/machinery/v1/tasks"
+)
+
+var cnf = &config.Config{
+  Broker:             "amqp://rabbitmq:rabbitmq@172.26.0.3:5672/",
+  DefaultQueue:       "machinery_tasks",
+  ResultBackend:      "amqp://rabbitmq:rabbitmq@172.26.0.3:5672/",
+  AMQP:               &config.AMQPConfig{
+    Exchange:     "machinery_exchange",
+    ExchangeType: "direct",
+    BindingKey:   "machinery_task",
+  },
+}
+
+func Add(args ...int64) (int64, error) {
+  sum := int64(0)
+  for _, arg := range args {
+    sum += arg
+  }
+  return sum, nil
+}
+
+func listen(server *machinery.Server) {
+    worker := server.NewWorker("worker_name", 10)
+    err := worker.Launch()
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func send(server *machinery.Server) {
+    signature := &tasks.Signature{
+      Name: "add",
+      Args: []tasks.Arg{
+        {
+          Type:  "int64",
+          Value: 1,
+        },
+        {
+          Type:  "int64",
+          Value: 1,
+        },
+      },
+    }
+
+    asyncResult, err_r := server.SendTask(signature)
+    if err_r != nil {
+      fmt.Println(err_r)
+    }
+
+    results, err_as := asyncResult.Get(time.Duration(time.Millisecond * 5))
+        if err_as != nil {
+                fmt.Errorf("Getting task result failed with error: %s", err_as.Error())
+        }
+    fmt.Println(tasks.HumanReadableResults(results))
+}
+
+func main() {
+    server, err := machinery.NewServer(cnf)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    server.RegisterTask("add", Add)
+
+    go send(server)
+    listen(server)
+}
