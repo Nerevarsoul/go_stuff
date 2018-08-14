523cab6d71e54adf3175f5a3cbb8490a409fce5c
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-08-14 08:29:41 +0300

    add http server for machinery example

--- packets/simple_machinery/main.go
+++ packets/simple_machinery/main.go
@@ -1,76 +1,87 @@
 package main
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 
-  "github.com/RichardKnop/machinery/v1/config"
-  "github.com/RichardKnop/machinery/v1"
-  "github.com/RichardKnop/machinery/v1/tasks"
+	"github.com/RichardKnop/machinery/v1"
+	"github.com/RichardKnop/machinery/v1/config"
+	"github.com/RichardKnop/machinery/v1/tasks"
 )
 
 var cnf = &config.Config{
-  Broker:             "amqp://rabbitmq:rabbitmq@172.26.0.3:5672/",
-  DefaultQueue:       "machinery_tasks",
-  ResultBackend:      "amqp://rabbitmq:rabbitmq@172.26.0.3:5672/",
-  AMQP:               &config.AMQPConfig{
-    Exchange:     "machinery_exchange",
-    ExchangeType: "direct",
-    BindingKey:   "machinery_task",
-  },
+	Broker:        "amqp://rabbitmq:rabbitmq@172.26.0.3:5672/",
+	DefaultQueue:  "machinery_tasks",
+	ResultBackend: "amqp://rabbitmq:rabbitmq@172.26.0.3:5672/",
+	AMQP: &config.AMQPConfig{
+		Exchange:     "machinery_exchange",
+		ExchangeType: "direct",
+		BindingKey:   "machinery_task",
+	},
 }
 
 func Add(args ...int64) (int64, error) {
-  sum := int64(0)
-  for _, arg := range args {
-    sum += arg
-  }
-  return sum, nil
+	sum := int64(0)
+	for _, arg := range args {
+		sum += arg
+	}
+	return sum, nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	server, err := machinery.NewServer(cnf)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	signature := &tasks.Signature{
+		Name: "add",
+		Args: []tasks.Arg{
+			{
+				Type:  "int64",
+				Value: 1,
+			},
+			{
+				Type:  "int64",
+				Value: 1,
+			},
+		},
+	}
+
+	asyncResult, err_r := server.SendTask(signature)
+	if err_r != nil {
+		fmt.Println(err_r)
+	}
+
+	results, err_as := asyncResult.Get(time.Duration(time.Millisecond * 5))
+	if err_as != nil {
+		fmt.Errorf("Getting task result failed with error: %s", err_as.Error())
+	}
+	fmt.Println(tasks.HumanReadableResults(results))
 }
 
 func listen(server *machinery.Server) {
-    worker := server.NewWorker("worker_name", 10)
-    err := worker.Launch()
-    if err != nil {
-        fmt.Println(err)
-    }
+	worker := server.NewWorker("worker_name", 10)
+	err := worker.Launch()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
-func send(server *machinery.Server) {
-    signature := &tasks.Signature{
-      Name: "add",
-      Args: []tasks.Arg{
-        {
-          Type:  "int64",
-          Value: 1,
-        },
-        {
-          Type:  "int64",
-          Value: 1,
-        },
-      },
-    }
-
-    asyncResult, err_r := server.SendTask(signature)
-    if err_r != nil {
-      fmt.Println(err_r)
-    }
-
-    results, err_as := asyncResult.Get(time.Duration(time.Millisecond * 5))
-        if err_as != nil {
-                fmt.Errorf("Getting task result failed with error: %s", err_as.Error())
-        }
-    fmt.Println(tasks.HumanReadableResults(results))
+func runServer() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":9002", nil))
 }
 
 func main() {
-    server, err := machinery.NewServer(cnf)
-    if err != nil {
-        fmt.Println(err)
-    }
+	server, err := machinery.NewServer(cnf)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    server.RegisterTask("add", Add)
-
-    go send(server)
-    listen(server)
+	go runServer()
+	server.RegisterTask("add", Add)
+	listen(server)
 }
