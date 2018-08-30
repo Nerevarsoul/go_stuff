1f85f3724944282113e253ad926665724794acdb
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-08-30 08:33:28 +0300

    fuction for socketio events

--- packets/chat/main.go
+++ packets/chat/main.go
@@ -7,25 +7,33 @@ import (
 	"github.com/googollee/go-socket.io"
 )
 
+func on_connection(so socketio.Socket) {
+	log.Println("on connection")
+	so.Join("chat")
+}
+
+func on_chat_message(so socketio.Socket, msg string) {
+	log.Println(msg)
+	so.BroadcastTo("chat", "chat message", msg)
+}
+
+func on_disconnect(so socketio.Socket) {
+	log.Println("on disconnect")
+}
+
+func on_error(so socketio.Socket, err error) {
+	log.Println("error:", err)
+}
+
 func main() {
 	server, err := socketio.NewServer(nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	server.On("connection", func(so socketio.Socket) {
-		log.Println("on connection")
-		so.Join("chat")
-	})
-	server.On("chat message", func(so socketio.Socket, msg string) {
-		log.Println(msg)
-		so.BroadcastTo("chat", "chat message", msg)
-	})
-	server.On("disconnection", func(so socketio.Socket) {
-		log.Println("on disconnect")
-	})
-	server.On("error", func(so socketio.Socket, err error) {
-		log.Println("error:", err)
-	})
+	server.On("connection", on_connection)
+	server.On("chat message", on_chat_message)
+	server.On("disconnection", on_disconnect)
+	server.On("error", on_error)
 
 	http.Handle("/socket.io/", server)
 	log.Println("Serving at localhost:5000...")
